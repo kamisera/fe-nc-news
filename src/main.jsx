5df0412890d22a9a60af65bdeec6926a3f5576f2
Kam Singh <kamisera+github@gmail.com>
2023-05-31 20:44:33 +0200

    adds logged in user provider

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { UserProvider } from "./contexts/User.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ToastContainer
@@ -19,7 +21,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       theme="dark"
     />
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
