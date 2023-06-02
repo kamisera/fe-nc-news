4de913b04f733c26e78ea0cdb9ebde44dd06f14d
Kam Singh <kamisera+github@gmail.com>
2023-06-02 11:43:48 +0200

    adds dynamic error message

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
-const PageNotFound = () => {
+const PageNotFound = ({ category }) => {
   return (
     <>
-      <h2>⚠️ OOPS ⚠️</h2>
-      <p>The page you requested could not be found!</p>
+      <h2 className="mb-4 pb-4">
+        <span className="pb-4 mb-4 border-bottom">⚠️ OOPS ⚠️</span>
+      </h2>
+      <p>The requested {category ?? "page"} could not be found!</p>
       <p>
         Click <Link to="/">here</Link> to go back to the homepage.
       </p>
