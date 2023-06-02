b5b168c4af1c1286e74842365cfa0bbfd03fe6df
Kam Singh <kamisera+github@gmail.com>
2023-06-02 12:34:31 +0200

    removes placeholder text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,38 +8,9 @@ const Home = () => {
         topics from technology to politics.
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis natus
-        eaque libero corporis assumenda odit mollitia molestiae beatae, dolore
-        rem saepe distinctio non vel sapiente quam. Nihil optio labore ratione?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-        corrupti voluptate dolores, accusamus sequi voluptates deleniti at error
-        repellat nulla, natus odio quos ea eaque animi exercitationem possimus
-        quam. Cumque ipsa, numquam asperiores doloribus eius dicta provident,
-        porro, fugiat assumenda facere necessitatibus repellendus reiciendis ab
-        blanditiis nemo fugit voluptatum quisquam vitae officiis ipsum quaerat
-        iusto enim ducimus? Nemo facilis ex, dignissimos voluptatem illo
-        accusamus culpa corrupti exercitationem laborum rem, tempore facere
-        consequuntur id error ab eaque quo maiores nulla ad. Quae hic tempore
-        nam quaerat alias aliquam officiis recusandae ea omnis magnam molestiae
-        tempora deleniti, molestias libero doloremque, architecto nobis eum
-        dolorem sed vero dicta optio. Sunt similique animi dolorem sint, nobis
-        ipsam placeat quae, consequatur aut, tempore possimus. Ut expedita
-        repudiandae alias labore sequi magnam sint harum porro. Voluptatum
-        dignissimos aut fugiat tenetur illo a voluptate explicabo sunt corporis
-        temporibus, quos doloribus repellendus repudiandae quia fuga dolorem
-        quasi dicta optio. Eum amet odit molestias ullam! Quod accusamus ducimus
-        officia enim sed, magni hic possimus blanditiis fugit aspernatur
-        pariatur itaque voluptatibus atque ea similique tenetur quibusdam harum
-        animi vitae nobis vero? Eius excepturi animi minima ex eveniet labore
-        consequuntur id magnam, perferendis cupiditate error facilis. Recusandae
-        dolore exercitationem maxime consequatur.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis natus
-        eaque libero corporis assumenda odit mollitia molestiae beatae, dolore
-        rem saepe distinctio non vel sapiente quam. Nihil optio labore ratione?
+        At NewsHub, you can browse, comment and vote on articles. Articles can
+        be filtered by clicking on one of the topics in the topics page. You can
+        sort articles by date, votes and number of comments.
       </p>
     </>
   );
